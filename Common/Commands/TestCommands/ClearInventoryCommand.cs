3d7f0b570aed94e2bfeb8de9f83524df100b5762
Cronus <cronuswaters@gmail.com>
2022-07-26 19:46:35 -0300

    Pushed test items to dev branch

---
+++ Common/Commands/TestCommands/ClearInventoryCommand.cs
@@ -0,0 +1,21 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace TerrariaFlagRandomizer.Common.Commands.TestCommands
+{
+    internal class ClearInventoryCommand : ModCommand
+    {
+        public override CommandType Type => CommandType.Chat;
+        public override string Command => "clear";
+        public override string Description => "Clears all non-favorited items from your inventory. Debug only.";
+        public override string Usage => "/clear";
+        public override void Action(CommandCaller caller, string input, string[] args)
+        {
+            Player callerPlayer = caller.Player;
+            foreach(Item item in callerPlayer.inventory)
+            {
+                if (!item.favorited) item.TurnToAir();
+            }
+        }
+    }
+}
---
+++ Common/Commands/TestCommands/TestRewardsCommand.cs
@@ -0,0 +1,32 @@
+﻿using Terraria.ModLoader;
+
+namespace TerrariaFlagRandomizer.Common.Commands.TestCommands
+{
+    public class TestRewardsCommand : ModCommand
+    {
+        public override CommandType Type => CommandType.Chat;
+        public override string Command => "flag";
+        public override string Description => "Cals the SetFlag method with the given args. Debug only.";
+        public override string Usage => "/flag flag_id [from_boss]";
+        public override void Action(CommandCaller caller, string input, string[] args)
+        {
+            if(args.Length == 0)
+            {
+                throw new UsageException("Argument flag_id is required");
+            }
+            if(!int.TryParse(args[0], out int id))
+            {
+                throw new UsageException(args[0] + " is not an integer");
+            }
+            if(id < 1 || id > 5)
+            {
+                throw new UsageException("Argument flag_id is outside the allowed range (1..5)");
+            }
+            if(args.Length < 2 || !bool.TryParse(args[1], out bool flag))
+            {
+                flag = false;
+            }
+            RewardsHandler.SetFlag(id, flag);
+        }
+    }
+}
